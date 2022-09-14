f1a9f338fda311af36e7f394278bced006c8c376
MSI\我 <anping6630@gmail.com>
2022-09-14 11:26:49 +0800

    未操作中機器人加入

--- Assets/CodeTest/DefaultRobot.cs
+++ Assets/CodeTest/DefaultRobot.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstPersonCamera : MonoBehaviour
+public class DefaultRobot : MonoBehaviour
 {
+    [Header("角色控制器")]
+    public CharacterController controller;
     [Header("攝影機")]
     public GameObject robotCamera;
-    [Header("滑鼠靈敏度")]
+    [Header("視角靈敏度")]
     public float mouseSensitivity;
+    [Header("移動速度")]
+    public float speed;
+    [Header("操作中")]
+    public bool isControlling;
 
     float xRotation = 0f;
 
@@ -18,6 +24,16 @@ public class FirstPersonCamera : MonoBehaviour
 
     void Update()
     {
+        if (isControlling)
+        {
+            LookFor();
+            Movement();
+        }
+        robotCamera.SetActive(isControlling);
+    }
+
+    void LookFor()
+    {   
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -26,5 +42,15 @@ public class FirstPersonCamera : MonoBehaviour
 
         robotCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
+    }
+
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
