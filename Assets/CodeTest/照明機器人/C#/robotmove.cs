114cface51a0c5cb420aa02d4c0d6f38358f6928
Tiffany Lee <u10914222@ms.ttu.edu.tw>
2022-10-24 15:50:08 +0800

    程式合體

---
+++ Assets/CodeTest/照明機器人/C#/robotmove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robotmove : MonoBehaviour
+{
+    public float speed=3.0f;
+    public float rotatespeed = 1.0f;
+    public float speed_x_constraction;
+    private Transform tran;
+    CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tran = gameObject.GetComponent<Transform>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.Rotate(0, h * rotatespeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * v;
+       
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+}
